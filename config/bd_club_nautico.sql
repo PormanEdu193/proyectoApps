f01a1bd0a3d0e9fece7fa76b6c6d65b3edd5a86d
porman193 <porman193@gmail.com>
2023-10-27 12:44:52 -0500

    Estructura del proyecto

---
+++ config/bd_club_nautico.sql
@@ -0,0 +1,168 @@
+
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+--
+-- Base de datos: `bd_club_nautico`
+--
+DROP SCHEMA IF EXISTS `bd_club_nautico` ;
+
+-- -----------------------------------------------------
+-- Schema bd_club_nautico
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `bd_club_nautico` ;
+USE `bd_club_nautico` ;
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Barcos`
+--
+
+CREATE TABLE `Barcos` (
+  `id_barco` int(11) NOT NULL,
+  `matricula` varchar(45) DEFAULT NULL,
+  `nombre_barco` varchar(45) DEFAULT NULL,
+  `numero_amarre` int(11) DEFAULT NULL,
+  `cuota` varchar(45) DEFAULT NULL,
+  `id_socio` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Patrones`
+--
+
+CREATE TABLE `Patrones` (
+  `id_patron` int(11) NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `identificacion` varchar(45) DEFAULT NULL,
+  `direccion` varchar(45) DEFAULT NULL,
+  `telefono` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Salidas`
+--
+
+CREATE TABLE `Salidas` (
+  `id_salida` int(11) NOT NULL,
+  `fecha_salida` date DEFAULT NULL,
+  `hora_salida` time DEFAULT NULL,
+  `destino` varchar(45) DEFAULT NULL,
+  `id_barco` int(11) NOT NULL,
+  `id_patron` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Socios`
+--
+
+CREATE TABLE `Socios` (
+  `id_socio` int(11) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `identificacion` varchar(45) DEFAULT NULL,
+  `direccion` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `telefono` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Socios_Usuarios`
+--
+
+CREATE TABLE `Socios_Usuarios` (
+  `id_socio` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Usuarios`
+--
+
+CREATE TABLE `Usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `contraseña` varchar(45) DEFAULT NULL,
+  `rol` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `Barcos`
+--
+ALTER TABLE `Barcos`
+  ADD PRIMARY KEY (`id_barco`),
+  ADD KEY `fk_Barcos_Socios_idx` (`id_socio`);
+
+--
+-- Indices de la tabla `Patrones`
+--
+ALTER TABLE `Patrones`
+  ADD PRIMARY KEY (`id_patron`);
+
+--
+-- Indices de la tabla `Salidas`
+--
+ALTER TABLE `Salidas`
+  ADD PRIMARY KEY (`id_salida`),
+  ADD KEY `fk_Salidas_Barcos1_idx` (`id_barco`),
+  ADD KEY `fk_Salidas_Patrones1_idx` (`id_patron`);
+
+--
+-- Indices de la tabla `Socios`
+--
+ALTER TABLE `Socios`
+  ADD PRIMARY KEY (`id_socio`);
+
+--
+-- Indices de la tabla `Socios_Usuarios`
+--
+ALTER TABLE `Socios_Usuarios`
+  ADD PRIMARY KEY (`id_socio`,`id_usuario`),
+  ADD KEY `fk_Socios_has_Usuarios_Usuarios1_idx` (`id_usuario`),
+  ADD KEY `fk_Socios_has_Usuarios_Socios1_idx` (`id_socio`);
+
+--
+-- Indices de la tabla `Usuarios`
+--
+ALTER TABLE `Usuarios`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- Restricciones para tablas volcadas
+--
+--
+-- Filtros para la tabla `Barcos`
+--
+ALTER TABLE `Barcos`
+  ADD CONSTRAINT `fk_Barcos_Socios` FOREIGN KEY (`id_socio`) REFERENCES `Socios` (`id_socio`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `Salidas`
+--
+ALTER TABLE `Salidas`
+  ADD CONSTRAINT `fk_Salidas_Barcos1` FOREIGN KEY (`id_barco`) REFERENCES `Barcos` (`id_barco`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_Salidas_Patrones1` FOREIGN KEY (`id_patron`) REFERENCES `Patrones` (`id_patron`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `Socios_Usuarios`
+--
+ALTER TABLE `Socios_Usuarios`
+  ADD CONSTRAINT `fk_Socios_has_Usuarios_Socios1` FOREIGN KEY (`id_socio`) REFERENCES `Socios` (`id_socio`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_Socios_has_Usuarios_Usuarios1` FOREIGN KEY (`id_usuario`) REFERENCES `Usuarios` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
