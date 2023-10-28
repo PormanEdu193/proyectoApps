b0661123e1cb79d62b16ae76926d44df38be1613
Deynner Sanabria <rojasdeynner8@gmail.com>
2023-10-28 14:08:37 -0500

    [Interfaz terminada] Frontend pagina principal

---
+++ views/usuario/Usuario__Pagina-principal.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../assets/css/reset.css">
+    <link rel="stylesheet" href="../../assets/css/body principal usuario.css">
+    <link rel="stylesheet" href="../../assets/css/Usuario__header.css">
+    <title>Pagina principal</title>
+</head>
+<body>
+    <header>
+        <h1 class="header__Nombre">CLUB NÁUTICO ALBATROS</h1>
+        <ul class="header__opciones">
+            <li><a href="">home</a></li>
+            <li><a href="./Usuario__Perfil.html">perfil</a></li>
+            <li><a href="./Usuario__barcos.html">barcos</a></li>
+            <li><a href="./Usuario__Salidas.html">historial de salidas</a></li>
+            <li><a href="../../index.html">salir</a></li>
+        </ul>
+    </header>
+    <main>
+        <div class="logoContainer">
+            <img src="../../assets/images/ship_white.png" alt="Silueta de un barco">
+        </div>
+    </main>
+    <div class="capa"></div>
+
+</body>
+</html>
